286bbf5900bfb0b200b8b6b3a2e249f1a4b89e84
Alysson Silva <alyssonext@gmail.com>
2025-12-03 22:38:48 -0300

    Chore: Fixiing the PORT Config again 2

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { app } from "./app"
 import { env } from "./env"
 
+const port = env.PORT || 3333
 
 app.listen({
-    port:  env.PORT || 3333
+  port,
+  host: '0.0.0.0',
 }).then(() => {
-    console.log('Server running')
+  console.log(`Server running on port ${port}`)
 })
-
